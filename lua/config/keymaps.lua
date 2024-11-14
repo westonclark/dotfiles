b866fc7b00783696e44bf67cdb6a109e93ae4321
Weston Clark <117543803+westonclark@users.noreply.github.com>
2024-11-14 09:34:33 -0800

    map esc to save

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- set leader w to save file
--- vim.keymap.set({ "i", "x", "n", "s" }, "<leader>w", ":w<CR>", { noremap = true, silent = true })
+--
+vim.keymap.set({ "i", "x", "n", "s" }, "<Esc>", "<Esc>:w<CR>", { noremap = true, silent = true })
