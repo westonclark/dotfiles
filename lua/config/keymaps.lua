c470bd6f1aaadfe29b9f28a0ef129068cebbb53b
Weston Clark <117543803+westonclark@users.noreply.github.com>
2024-12-02 09:59:29 -0800

    update move line keymaps, add go lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- set leader w to save file
 --
+-- Save on esc
 vim.keymap.set({ "i", "x", "n", "s" }, "<Esc>", "<Esc>:w<CR>", { noremap = true, silent = true })
+
+-- Close buffer with <Leader>w
 vim.keymap.set("n", "<Leader>w", "<Cmd>bdelete<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "K", ":move -2<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "J", ":move +1<CR>", { noremap = true, silent = true })
+
+-- Normal mode: move lines
+vim.keymap.set("n", "K", ":move -2<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "J", ":move +1<cr>", { noremap = true, silent = true })
+
+-- Visual mode: move lines and maintain visual selection
+vim.keymap.set("v", "K", ":move '<-2<cr>gv", { noremap = true, silent = true })
+vim.keymap.set("v", "J", ":move '>+1<cr>gv", { noremap = true, silent = true })
