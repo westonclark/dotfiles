a01eb5f6172f606d2854a7a67576576cc6c8c332
Weston Clark <117543803+westonclark@users.noreply.github.com>
2025-05-12 11:09:46 -0700

    remove lualine

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,12 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    window = { position = "right" },
+    window = { position = "float" },
+    popup_border_style = "rounded",
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
   },
 }
