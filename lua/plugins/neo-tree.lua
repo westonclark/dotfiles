808f4d59924e205d7f736f7d57e297962f5cefdf
Weston Clark <westoclark@gmail.com>
2025-12-29 13:23:51 -0800

    Use neotree reveal shortcut

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("neo-tree.command").execute({ action = "focus", reveal = true })
+      end,
+      desc = "Explorer NeoTree (reveal current file)",
+    },
+  },
   opts = {
     window = { position = "float" },
     popup_border_style = "rounded",
