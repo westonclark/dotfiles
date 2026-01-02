562f7081444ebe3c2fee93957dcda77c23eb7433
Weston Clark <westoclark@gmail.com>
2026-01-01 22:55:26 -0800

    Remove mini pairs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.snacks_scroll = false
-vim.opt.laststatus = 0  -- Completely disable the statusline
-vim.opt.cursorline = false  -- Disable current line highlight
+vim.g.snacks_scroll = false -- disable scroll animation
+vim.opt.laststatus = 0 -- Completely disable the statusline
+vim.opt.cursorline = false -- Disable current line highlight
