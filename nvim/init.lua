fb2ba97a21a77af680528bde47cea1ddeb6bb0c9
Weston Clark <westoclark@gmail.com>
2026-02-10 21:32:49 -0800

    Add case insensitive search mini.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,26 +38,30 @@ vim.opt.signcolumn = "yes"
 vim.opt.winborder = "rounded"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -------------------------------------------------------------------
 -- KEYMAPS --
 -------------------------------------------------------------------
 -- Save on esc and clear search highlights
-vim.keymap.set({ "i", "x", "n", "s" }, "<Esc>", "<Esc>:noh | silent! w<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<Esc>", "<Esc>:noh | silent! w<CR>", { silent = true })
 
 -- Close buffer
-vim.keymap.set("n", "<Leader>w", "<Cmd>bdelete!<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>w", "<Cmd>bdelete!<CR>", { silent = true })
 
 -- Quit
-vim.keymap.set("n", "<Leader>q", "<Cmd>:quit<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>q", "<Cmd>quit<CR>", { silent = true })
 
 -- Normal mode: move lines
-vim.keymap.set("n", "K", ":move -2<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "J", ":move +1<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "K", ":move -2<cr>", { silent = true })
+vim.keymap.set("n", "J", ":move +1<cr>", { silent = true })
 
 -- Visual mode: move lines and maintain visual selection
-vim.keymap.set("v", "K", ":move '<-2<cr>gv", { noremap = true, silent = true })
-vim.keymap.set("v", "J", ":move '>+1<cr>gv", { noremap = true, silent = true })
+vim.keymap.set("v", "K", ":move '<-2<cr>gv", { silent = true })
+vim.keymap.set("v", "J", ":move '>+1<cr>gv", { silent = true })
 
 -- Keep cursor centered
 vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Page down, stay centered
@@ -69,114 +73,115 @@ vim.keymap.set("n", "N", "Nzzzv")       -- Prev search, stay centered
 -- COLORSCHEME --
 -------------------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
+  { src = "https://github.com/vague2k/vague.nvim" },
 })
-vim.cmd("colorscheme vague")
-vim.cmd(":hi statusline guibg=NONE")
+vim.cmd.colorscheme("vague")
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
 
 -------------------------------------------------------------------
 -- INDENT BLANKLINE (SCOPE LINES) --
 -------------------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+  { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
 })
 require("ibl").setup({
-	indent = {
-		char = "│",
-	},
-	scope = {
-		enabled = false,
-		show_start = false,
-		show_end = false,
-	},
+  scope = { enabled = false },
 })
 
 -------------------------------------------------------------------
 -- LSP --
 -------------------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
 })
 
 -- Configure lua_ls for Neovim
 vim.lsp.config.lua_ls = {
-	settings = {
-		Lua = {
-			runtime = { version = 'LuaJIT' },
-			workspace = {
-				library = { vim.env.VIMRUNTIME },
-				checkThirdParty = false
-			},
-		}
-	}
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT' },
+      workspace = {
+        library = { vim.env.VIMRUNTIME },
+        checkThirdParty = false
+      },
+    }
+  }
 }
 
 vim.lsp.enable({
-	"lua_ls", -- Lua
-	"clangd", -- C/C++
-	"ts_ls",  -- TypeScript/JavaScript
-	"rust_analyzer", -- Rust
-	"gopls",  -- Go
-	"marksman", -- Markdown
-	"pyright", -- Python
+  "lua_ls",        -- Lua
+  "clangd",        -- C/C++
+  "ts_ls",         -- TypeScript/JavaScript
+  "rust_analyzer", -- Rust
+  "gopls",         -- Go
+  "marksman",      -- Markdown
+  "pyright",       -- Python
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		vim.keymap.set("n", "H", vim.lsp.buf.hover, { noremap = true, silent = true })
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, noremap = true, silent = true })
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf })
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = ev.buf })
-		vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = ev.buf })
-		vim.keymap.set("n", "<Leader>f", vim.lsp.buf.format, { noremap = true, silent = true })
-		-- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = ev.buf })
-		-- vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer = ev.buf })
-	end,
+  callback = function(ev)
+    vim.keymap.set("n", "H", vim.lsp.buf.hover, { buffer = ev.buf, silent = true })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, silent = true })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, silent = true })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = ev.buf, silent = true })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = ev.buf, silent = true })
+    vim.keymap.set("n", "<Leader>f", vim.lsp.buf.format, { buffer = ev.buf, silent = true })
+  end,
 })
 
 -------------------------------------------------------------------
 -- BLINK LSP AUTOCOMPLETE --
 -------------------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/saghen/blink.cmp", version = "v0.7.6" }
+  { src = "https://github.com/saghen/blink.cmp", version = "v0.7.6" }
 })
 require('blink.cmp').setup({
-	keymap = {
-		['<Tab>'] = { 'accept', 'fallback' },
-	},
-	completion = {
-		ghost_text = { enabled = true },
-		menu = { enabled = false },
-		documentation = { enabled = false },
-	},
+  keymap = {
+    ['<Tab>'] = { 'select_and_accept', 'snippet_forward', 'fallback' },
+  },
+  completion = {
+    ghost_text = { enabled = true },
+    menu = {
+      border = 'rounded',
+      draw = {
+        columns = { { "label" } },
+      },
+    },
+    documentation = {
+      auto_show = false,
+    },
+  },
+  appearance = {
+    nerd_font_variant = 'mono'
+  },
 })
 
 -------------------------------------------------------------------
 -- MINI PICK FILE SEARCH --
 -------------------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/echasnovski/mini.pick" },
+  { src = "https://github.com/echasnovski/mini.pick" },
 })
 require('mini.pick').setup({
-	mappings = {
-		move_down = '<C-j>',
-		move_up = '<C-k>',
-	},
-	window = {
-		config = function()
-			local height = math.floor(0.618 * vim.o.lines)
-			local width = math.floor(0.618 * vim.o.columns)
-			return {
-				anchor = 'NW',
-				height = height,
-				width = width,
-				row = math.floor(0.5 * (vim.o.lines - height)),
-				col = math.floor(0.5 * (vim.o.columns - width)),
-			}
-		end,
-	},
+  mappings = {
+    move_down = '<C-j>',
+    move_up = '<C-k>',
+  },
+  window = {
+    config = function()
+      local height = math.floor(0.618 * vim.o.lines)
+      local width = math.floor(0.618 * vim.o.columns)
+      return {
+        anchor = 'NW',
+        height = height,
+        width = width,
+        row = math.floor(0.5 * (vim.o.lines - height)),
+        col = math.floor(0.5 * (vim.o.columns - width)),
+      }
+    end,
+  },
 })
-vim.keymap.set("n", "<Leader><space>", "<Cmd>:Pick files<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader><space>", "<Cmd>Pick files<CR>", { silent = true })
 vim.keymap.set('n', '<leader>b', require('mini.pick').builtin.buffers)
 vim.keymap.set("n", "<Leader>/", require('mini.pick').builtin.grep_live)
 
@@ -184,32 +189,33 @@ vim.keymap.set("n", "<Leader>/", require('mini.pick').builtin.grep_live)
 -- OIL FILE EXPLORER--
 -------------------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/stevearc/oil.nvim" },
 })
-vim.keymap.set("n", "<Leader>e", "<Cmd>Oil --float<CR>", { noremap = true, silent = true })
 require("oil").setup({
-	float = {
-		padding = 2,
-		max_width = math.floor(0.618 * vim.o.columns),
-		max_height = math.floor(0.618 * vim.o.lines),
-		border = "rounded",
-	},
-	view_options = {
-		show_hidden = true,
-	},
-	keymaps = {
-		["<Esc>"] = "actions.close",
-		["l"] = "actions.select",
-		["h"] = "actions.parent",
-		["H"] = "actions.toggle_hidden",
-	},
+  float = {
+    padding = 2,
+    max_width = math.floor(0.618 * vim.o.columns),
+    max_height = math.floor(0.618 * vim.o.lines),
+    border = "rounded",
+  },
+  view_options = {
+    show_hidden = true,
+  },
+  keymaps = {
+    ["<Esc>"] = "actions.close",
+    ["l"] = "actions.select",
+    ["h"] = "actions.parent",
+    ["H"] = "actions.toggle_hidden",
+    ["yp"] = "actions.copy_entry_path",
+  },
 })
+vim.keymap.set("n", "<Leader>e", "<Cmd>Oil --float<CR>", { silent = true })
 
 -------------------------------------------------------------------
 -- GITSIGNS --
 -------------------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+  { src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
 require('gitsigns').setup()
 
@@ -217,6 +223,6 @@ require('gitsigns').setup()
 -- LAZYGIT --
 -------------------------------------------------------------------
 vim.pack.add({
-	{ src = "https://github.com/kdheepak/lazygit.nvim" },
+  { src = "https://github.com/kdheepak/lazygit.nvim" },
 })
-vim.keymap.set("n", "<leader>g", "<cmd>LazyGit<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>g", "<cmd>LazyGit<cr>", { silent = true })
